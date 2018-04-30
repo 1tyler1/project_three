0e79b7a8c14336596a36bef67b8eda68f40fd836
1tyler1 <mya12ga@yahoo.com>
2018-04-30 16:39:39 -0400

    updating work

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import styles from './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
 
 import HomePage from './components/HomePage'
 import LogInPage from './components/LogInPage'
--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -6,9 +6,25 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 class HomePage extends Component {
   render(){
     return (
-      <div>
-
-          <Link to='/login'>Login Link</Link>
+     
+<div class="row">
+  <div class="col s6 m3">
+    <h2 class="header center">Progress!!</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="../../public/action-athlete-barbell-841130.jpg" />
+      
+      <div class="card-stacked">
+        <div class="card-content">
+          <p class="center">Progress is app an that allows you to visualize your progression, whether weight loss or making gains. Best wishes on your journey to changing your body for the best!</p>
+        </div>
+        <div class="card-action">
+        <Link class="waves-effect waves-light btn" to='/login'>Login Link</Link>
+        </div>
+      </div>
+    </div>
+  </div>
+     </div>     
 
       </div>
     )
